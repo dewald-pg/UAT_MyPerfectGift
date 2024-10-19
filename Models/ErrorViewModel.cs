414abe81d03b24b2370362343aa8df889d25e06d
dewald-pg <dewald2basson2@gmail.com>
2024-10-19 08:56:04 +0200

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyPerfectGift.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿/*Landing Page Start*/
+
+/*Hide/Show Description Boxes*/
+function showDescription(iconNumber) {
+    hideAllDescriptions();
+    document.getElementById(`landing-description-icon-${iconNumber}`).classList.add('active');
+}
+
+function hideDescription(iconNumber) {
+    document.getElementById(`landing-description-icon-${iconNumber}`).classList.remove('active');
+}
+
+function hideAllDescriptions() {
+    const descriptions = document.querySelectorAll('.description-text');
+    descriptions.forEach(description => description.classList.remove('active'));
+}
+
+
+/*Landing Page End*/
